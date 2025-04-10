534dc14ce00cd8d07b98dfae4dbbe8776a82f2be
Khrystyna <penyakkristi@gmail.com>
2025-04-10 14:12:31 +0300

    Lab3: add limit offset pagination and db

---
+++ lab3/models.py
@@ -0,0 +1,12 @@
+from flask_sqlalchemy import SQLAlchemy
+import uuid
+
+db = SQLAlchemy()
+
+class Book(db.Model):
+    __tablename__ = 'books'
+
+    id = db.Column(db.String(36), primary_key=True, default=lambda: str(uuid.uuid4()))
+    title = db.Column(db.String(255), nullable=False)
+    author = db.Column(db.String(255), nullable=False)
+    year = db.Column(db.Integer, nullable=False)
